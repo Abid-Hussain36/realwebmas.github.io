98a2b65cf715c2d467c88faaaef5eac0c5347fa9
Pathu05 <69016300+Pathu05@users.noreply.github.com>
2022-02-10 21:22:48 -0500

    Update AussieShow.js

--- AussieShow.js
+++ AussieShow.js
@@ -6,15 +6,24 @@ const AussieShow = () => {
   const aussieData =[
     {
       countryName: "Australia",
-      demographics: "88% are Native Spaniards",
-      culture: "5 meal system",
-      food: "eat lots of pasta (non)"
+      demographics: "There are English (25.9%), Australian (25.4%), Irish (7.5%), Scottish (6.4%), Italian (3.3%), German (3.2%), Chinese (3.1%), Dutch (1.2%), and other ethinic groups (15.8%), in Australia.",
+      language: "The most spoken language of Australia is English.",
+      festivals: "Some prominent festivals are Sydney Mardi Gras, Dreaming Festival, and Brisbane Festival.",
+      sources: "Index Mundi, iExplore",
     },
     {
       countryName: "New Zealand",
-      demographics: "90% are Germans",
-      culture: "no meal system",
-      food: "dismal"
+      demographics: "There are European (64.1%), Maori (16.5%), Chinese (4.9%), Indian (4.7%), Samaoan (3.9%), Tongan (1.8%), Cook Islands Maori (1.7%), English (1.5%), Filipino (1.5%), New Zealander (1%), and other ethinic groups (13.7%) in New Zealand",
+      language: "The most spoken language of New Zealand is English.",
+      festivals: "Some prominent festivals are Bluff Oyster & Food Festival, Matariki Festival, and Rally New Zealand.",
+      sources: "Index Mundi, iExplore",
+    },
+    {
+      countryName: "Fiji",
+      demographics: "There are iTaukei (56.8%), Indo-Fijian (37.5%), Rotuman (1.2%), and other ethinic groups (4.5%)",
+      language: "The most spoken language of Fiji is English.",
+      festivals: "Some prominent festivals are Outrigger International Competition, Honoring the Founder of Modern Fiji Festival, and Hibiscus Festival.",
+      sources: "Index Mundi, iExplore",
     }
   ];
 
